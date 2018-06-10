715df56a31e80a57bdf4b440f80b939242f59ad1
Joe Nagel <jnagel88@gmail.com>
2018-06-10 16:57:22 -0500

    set associations for crypto/user models.

--- app/models/crypto.rb
+++ app/models/crypto.rb
@@ -1,2 +1,4 @@
 class Crypto < ApplicationRecord
+  belongs_to :user
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-
   resources :cryptos
   devise_for :users
   get 'lookup' => "home#lookup"
